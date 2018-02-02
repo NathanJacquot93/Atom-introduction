3a9b49f1e24d2a6b15f10559f2a8103619ba5ead
Nathan <nj@agence-evolve.com>
2018-02-02 14:43:39 +0100

    Création de la page Biography.php

---
+++ biography.php
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <h1>Page Biographie</h1>
+    <h2>Page ajoutée via Atom</h2>
+
+    <div class="container">
+      <p>Lorem ipsum dolor etnis acapum.</p>
+    </div>
+   </body>
+</html>
